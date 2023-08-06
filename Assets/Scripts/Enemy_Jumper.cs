4409d71faa1b6fd577d2e5c7efbc2a5b5b340d16
MalykhRoman <alrytanks@gmail.com>
2023-08-06 22:42:53 +0300

    Новые варианты спавна пришельцев и новый тип пришельца - попрыгнун

--- Assets/Scripts/Enemy_Jumper.cs
+++ Assets/Scripts/Enemy_Jumper.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections;
 using UnityEngine;
 
 public class Enemy_Jumper : MonoBehaviour
@@ -7,17 +8,84 @@ public class Enemy_Jumper : MonoBehaviour
     [SerializeField] private int givenScore = 1;
     [SerializeField] private float speed;
 
-    private Vector2 targetPos;
+    [SerializeField] private bool isOnLine = true;
+
+    
+    [SerializeField] private Vector2 targetPos;
+
     [SerializeField] private float yIncrement;
+    [SerializeField] private Vector2 direction;
+
 
     [SerializeField] private DOTweenAnimation deathAnimation;
 
     [SerializeField] private GameObject deathEffect;
 
+    [SerializeField] private GameObject fireEffect;
+    [SerializeField] private GameObject fireObject;
+
+    private void Start()
+    {
+        //StartCoroutine(ChandgeLine());
+    }
+
     private void Update()
     {
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        //direction = new Vector2 (-1, Random.Range(-1, 2));
+        transform.Translate(direction * speed * Time.deltaTime);
+        IsOnLine();
+        //direction = new Vector2 (-1, 0);
+        IsOnLine();
+        if (isOnLine)
+        {
+            int rand = Random.Range(0, 3);
+            Debug.Log($"Random value: {rand}");
+
+            switch (rand)
+            {
+                case 0:
+                    targetPos = new Vector2(transform.position.x - 10, 0);
+                    Debug.Log("В нулях");
+                    break;
+                case 1:
+                    targetPos = new Vector2(transform.position.x - 10, yIncrement);
+                    break;
+                case 2:
+                    targetPos = new Vector2(transform.position.x - 10, -yIncrement);
+                    break;
+                default:
+                    targetPos = new Vector2(transform.position.x - 10, 0);
+                    break;
+            }
+            //Instantiate(fireEffect, fireObject.transform);
+
+        }
+        //transform.Translate(Vector2.left * speed * Time.deltaTime);
+        //transform.Translate(direction * speed * Time.deltaTime);
+
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+    }
+
+    private void IsOnLine()
+    {
+        if ((transform.position.y == yIncrement) || (transform.position.y == -yIncrement) || (transform.position.y == 0f))
+        {
+            isOnLine = true;
+            //direction = new Vector2(-1, Random.Range(-1, 2));
+            //if (transform.position.y < yIncrement)
+            //{
+            //    direction = new Vector2(-1, Random.Range(0, 2));
+            //}
+            //else if (transform.position.y > -yIncrement)
+            //{
+            //    direction = new Vector2(-1, Random.Range(-1, 1));
+            //}
+        }
+        else
+        {
+            isOnLine = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
